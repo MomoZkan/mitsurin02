07ee2dcae8924a9f18038fdcc152158e9aec5156
Kidosouta <Yuiuiyuuki1006>
2019-12-26 10:45:40 +0900

    エラー解消

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("emperor参上");
-<<<<<<< HEAD
         System.out.println("ypaaaaaaaaaa");
-=======
         System.out.println("Garuvan見参");
->>>>>>> master
     }
 }
