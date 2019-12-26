5d5fca6358160acb0d288c321d52bbb4003380b4
Kidosouta <Yuiuiyuuki1006>
2019-12-26 11:36:42 +0900

    characterクラスの追加

---
+++ src/com/company/character.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class character {
+    private String name;
+    public character(String name){
+        name=this.name;
+    }
+    public void Printname(){
+        System.out.println(name);
+    }
+}
