3de4062e0dac8fc9bae9863ad394294b53becdb3
e1b18034 <e1b18034@st.oit.ac.jp>
2019-12-26 11:58:44 +0900

    characterクラス修正
    代入ミスを修正しました

--- src/com/company/character.java
+++ src/com/company/character.java
@@ -3,7 +3,7 @@ package com.company;
 public class character {
     private String name;
     public character(String name){
-        name=this.name;
+        this.name=name;
     }
     public void Printname(){
         System.out.println(name);
